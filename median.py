1b7d2ce345e5d0167e20f6c32c4a21984cd23d4a
Patrick Groves <patrickalexgroves@gmail.com>
2021-06-21 17:12:46 -0600

    sorts num_list in median

--- median.py
+++ median.py
@@ -1,13 +1,14 @@
 # Requires sorted list
 
 def median(num_list):
-    middle_position = len(num_list) // 2
+    sorted_num_list = sorted(num_list)
+    middle_position = len(sorted_num_list) // 2
 
-    if len(num_list) % 2 == 1:
-        return num_list[middle_position]
+    if len(sorted_num_list) % 2 == 1:
+        return sorted_num_list[middle_position]
 
-    even_median_sum = num_list[middle_position] + \
-        num_list[middle_position - 1]
+    even_median_sum = sorted_num_list[middle_position] + \
+        sorted_num_list[middle_position - 1]
     even_median = even_median_sum / 2
     return even_median
 
@@ -16,4 +17,4 @@ def median(num_list):
 print(median([2, 2, 3, 3]))
 print(median([1, 2, 3, 4, 5]))
 print(median([2, 3, 4, 5, 6, 9, 12]))
-print(median([33, 33, 66, 77, 99, 108]))
+print(median([77, 33, 33, 66, 99, 108]))
